0bc5d84d4b7a2d6eadb467741c7342c9d1091be0
Michael Savelyev <msavelyev@yandex-team.ru>
2013-02-23 13:02:51 +0400

    Tank moves smoother

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,12 +1,17 @@
 import org.newdawn.slick._
-import scala.collection.mutable.Map
+import scala.collection.mutable
 
-class Game( gameClient: GameClient ) extends BasicGame( "Hello" ) with KeyListener  {
+class Game(gameClient: GameClient) extends BasicGame("Hello") with KeyListener {
+    
+    object MoveDirection extends Enumeration {
+        type MoveDirection = Value
+        val Up, Down, Left, Right, None = Value
+    }
 
     gameClient.game = this
     gameClient.connect()
 
-    var tank: Image = null
+    var tank: Image = _
 
     val scale = 1.0f
     val step = 26f
@@ -15,67 +20,78 @@ class Game( gameClient: GameClient ) extends BasicGame( "Hello" ) with KeyListen
 
     var x = 0.0f
     var y = 0.0f
+    val speed = 0.1f
 
-    val players = Map[Int, Player]()
+    val players = mutable.Map[Int, Player]()
+    var moveDirection = MoveDirection.None
 
     def update(container: GameContainer, delta: Int) {
-
+        moveDirection match {
+            case MoveDirection.Up => y -= speed * delta
+            case MoveDirection.Down => y += speed * delta
+            case MoveDirection.Left => x -= speed * delta
+            case MoveDirection.Right => x += speed * delta
+            case _ =>
+        }
     }
 
-    def connected( playerId: Int, x: Int, y: Int ) {
-        players( playerId ) = Player( playerId, x, y )
+    def connected(playerId: Int, x: Int, y: Int) {
+        players(playerId) = Player(playerId, x, y)
     }
 
-    def disconnected( playerId: Int ) {
+    def disconnected(playerId: Int) {
         players -= playerId
     }
 
-    def move( playerId: Int, dx: Int, dy: Int ) {
-        players( playerId ) = players( playerId ).move( dx, dy )
+    def move(playerId: Int, dx: Int, dy: Int) {
+        players(playerId) = players(playerId).move(dx, dy)
     }
 
     def init(container: GameContainer) {
-        tank = new Image( "tank.png" )
+        tank = new Image("tank.png")
 
-        container.getInput.addKeyListener( this )
+        container.getInput.addKeyListener(this)
     }
 
     def render(container: GameContainer, g: Graphics) {
-        g.setColor( Color.black )
-        g.fillRect( 0, 0, container.getWidth, container.getHeight )
+        g.setColor(Color.black)
+        g.fillRect(0, 0, container.getWidth, container.getHeight)
 
-        tank.draw( x, y, scale )
+        tank.draw(x, y, scale)
 
-        g.setColor( Color.white )
-        g.drawString( "" + ( x / step ).toInt + ";" + ( y / step ).toInt, 10, 25 )
+        g.setColor(Color.white)
+        g.drawString("" + (x / step).toInt + ";" + (y / step).toInt, 10, 25)
 
-        for( player <- players.values ) {
-            tank.draw( player.x * step, player.y * step )
+        for (player <- players.values) {
+            tank.draw(player.x * step, player.y * step)
+        }
+    }
+
+
+    override def keyPressed(key: Int, c: Char) {
+        key match {
+            case Input.KEY_UP => 
+                moveDirection = MoveDirection.Up
+                tank.rotate(0 - tank.getRotation)
+            case Input.KEY_DOWN =>
+                moveDirection = MoveDirection.Down
+                tank.rotate(180 - tank.getRotation)
+            case Input.KEY_LEFT =>
+                moveDirection = MoveDirection.Left
+                tank.rotate(270 - tank.getRotation)
+            case Input.KEY_RIGHT =>
+                moveDirection = MoveDirection.Right
+                tank.rotate(90 - tank.getRotation)
+            case _ =>
         }
     }
 
     override def keyReleased(key: Int, c: Char) {
-        key match {
-            case Input.KEY_UP => {
-                y -= step
-                tank.rotate( 0 - tank.getRotation )
-                gameClient.move( 0, -1 )
-            }
-            case Input.KEY_DOWN => {
-                y += step
-                tank.rotate( 180 - tank.getRotation )
-                gameClient.move( 0, 1 )
-            }
-            case Input.KEY_LEFT => {
-                x -= step
-                tank.rotate( 270 - tank.getRotation )
-                gameClient.move( -1, 0 )
-            }
-            case Input.KEY_RIGHT => {
-                x += step
-                tank.rotate( 90 - tank.getRotation )
-                gameClient.move( 1, 0 )
-            }
+        (key, moveDirection) match {
+            case (Input.KEY_UP, MoveDirection.Up) => moveDirection = MoveDirection.None 
+            case (Input.KEY_DOWN, MoveDirection.Down) => moveDirection = MoveDirection.None
+            case (Input.KEY_LEFT, MoveDirection.Left) => moveDirection = MoveDirection.None
+            case (Input.KEY_RIGHT, MoveDirection.Right) => moveDirection = MoveDirection.None
             case _ =>
         }
     }
@@ -83,11 +99,11 @@ class Game( gameClient: GameClient ) extends BasicGame( "Hello" ) with KeyListen
 
 object Game {
 
-    def main( args: Array[ String ] ) {
+    def main(args: Array[String]) {
         val gameClient = new GameClient
-        val app = new AppGameContainer( new Game( gameClient ) )
+        val app = new AppGameContainer(new Game(gameClient))
 
-        app.setDisplayMode( 800, 600, false )
+        app.setDisplayMode(800, 600, false)
         app.start()
     }
 
--- src/main/scala/GameClient.scala
+++ src/main/scala/GameClient.scala
@@ -14,16 +14,19 @@ class GameClient extends Loggable {
     var game: Game = null
     var client: BayeuxClient = null
 
+    var okay = false
+
     def connect() {
         val webSocketClientFactory: WebSocketClientFactory = new WebSocketClientFactory()
-        val transport = WebSocketTransport.create(Map[ String, Object ](), webSocketClientFactory)
+        val transport = WebSocketTransport.create(Map[String, Object](), webSocketClientFactory)
 
         client = new BayeuxClient("http://localhost:8080/cometd-test/cometd", transport)
 
         client.getChannel(Channel.META_HANDSHAKE).addListener(
-            ( channel: ClientSessionChannel, message: Message ) => {
-                if( message.isSuccessful ) {
+            (channel: ClientSessionChannel, message: Message) => {
+                if (message.isSuccessful) {
                     connected()
+                    okay = true
                 }
             }
         )
@@ -32,88 +35,90 @@ class GameClient extends Loggable {
     }
 
     def connected() {
-        client.getChannel( "/service/game/playerId" ).publish( Map[String, Any]( ) )
+        client.getChannel("/service/game/playerId").publish(Map[String, Any]())
 
-        client.getChannel( "/service/game/sendPlayerInfo" ).addListener(
-            ( channel: ClientSessionChannel, message: Message ) => {
+        client.getChannel("/service/game/sendPlayerInfo").addListener(
+            (channel: ClientSessionChannel, message: Message) => {
                 val data: Map[String, AnyRef] = message.getDataAsMap
                 INFO << "connected " + data
                 val playerId: Int = data("playerId").toString.toInt
-                if( playerId != game.playerId ) {
+                if (playerId != game.playerId) {
                     game.connected(
                         playerId,
-                        data( "x" ).toString.toInt,
-                        data( "y" ).toString.toInt
+                        data("x").toString.toInt,
+                        data("y").toString.toInt
                     )
                 }
             }
         )
 
-        client.getChannel( "/service/game/sendPlayerId" ).addListener(
-            ( channel: ClientSessionChannel, message: Message ) => {
-                val data: Map[ String, AnyRef ] = message.getDataAsMap
-                val players: Array[Any] = data( "players" ).asInstanceOf[Array[Any]]
-                for( player <- players ) {
-                    val data: JavaMap[String, Any] = Map[String, Any]( "playerId" -> player.toString.toInt )
+        client.getChannel("/service/game/sendPlayerId").addListener(
+            (channel: ClientSessionChannel, message: Message) => {
+                val data: Map[String, AnyRef] = message.getDataAsMap
+                val players: Array[Any] = data("players").asInstanceOf[Array[Any]]
+                for (player <- players) {
+                    val data: JavaMap[String, Any] = Map[String, Any]("playerId" -> player.toString.toInt)
                     INFO << "getting info of player " + data
-                    client.getChannel( "/service/game/playerInfo" ).publish( data )
+                    client.getChannel("/service/game/playerInfo").publish(data)
                 }
-                game.playerId = data( "playerId" ).toString.toInt
+                game.playerId = data("playerId").toString.toInt
                 INFO << "got playerId " + game.playerId
             }
         )
 
-        client.getChannel( "/game/move" ).subscribe(
-            ( channel: ClientSessionChannel, message: Message ) => {
+        client.getChannel("/game/move").subscribe(
+            (channel: ClientSessionChannel, message: Message) => {
                 val data: Map[String, AnyRef] = message.getDataAsMap
                 INFO << "move " + data
                 val playerId: Int = data("playerId").toString.toInt
-                if( playerId != game.playerId ) {
+                if (playerId != game.playerId) {
                     game.move(
                         playerId,
-                        data( "dx" ).toString.toInt,
-                        data( "dy" ).toString.toInt
+                        data("dx").toString.toInt,
+                        data("dy").toString.toInt
                     )
                 }
             }
         )
 
-        client.getChannel( "/game/connected" ).subscribe(
-            ( channel: ClientSessionChannel, message: Message ) => {
+        client.getChannel("/game/connected").subscribe(
+            (channel: ClientSessionChannel, message: Message) => {
                 val data: Map[String, AnyRef] = message.getDataAsMap
                 INFO << "connected " + data
                 val playerId: Int = data("playerId").toString.toInt
-                if( playerId != game.playerId ) {
+                if (playerId != game.playerId) {
                     game.connected(
                         playerId,
-                        data( "x" ).toString.toInt,
-                        data( "y" ).toString.toInt
+                        data("x").toString.toInt,
+                        data("y").toString.toInt
                     )
                 }
             }
         )
 
-        client.getChannel( "/game/disconnected" ).subscribe(
-            ( channel: ClientSessionChannel, message: Message ) => {
+        client.getChannel("/game/disconnected").subscribe(
+            (channel: ClientSessionChannel, message: Message) => {
                 val data: Map[String, AnyRef] = message.getDataAsMap
                 INFO << "disconnected " + data
                 val playerId: Int = data("playerId").toString.toInt
-                if( playerId != game.playerId ) {
-                    game.disconnected( playerId )
+                if (playerId != game.playerId) {
+                    game.disconnected(playerId)
                 }
             }
         )
     }
 
-    def move( dx: Int, dy: Int ) {
-        val data: JavaMap[String, Any] = Map[String, Any]( "dx" -> dx, "dy" -> dy )
-        client.getChannel( "/service/game/move" ).publish( data )
+    def move(dx: Int, dy: Int) {
+        if (okay) {
+            val data: JavaMap[String, Any] = Map[String, Any]("dx" -> dx, "dy" -> dy)
+            client.getChannel("/service/game/move").publish(data)
+        }
     }
 
-    implicit def messageListener( f: ( ClientSessionChannel, Message ) => Unit ): MessageListener = {
+    implicit def messageListener(f: (ClientSessionChannel, Message) => Unit): MessageListener = {
         new MessageListener {
             def onMessage(channel: ClientSessionChannel, message: Message) {
-                f( channel, message )
+                f(channel, message)
             }
         }
     }
